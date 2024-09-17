400cb55d5e9b25f9002109f1ea05e7dc1444fbb4
DervinEvolve <dimancherise@gmail.com>
2024-09-16 20:22:54 -0400

    Change 'address title and description'

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Empathy Archives",
+  description: "A Voice to Voice Historical Research Assistant that Analyzes Tone",
 };
 
 export default function RootLayout({
